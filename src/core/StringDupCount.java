1bf3fc4014a3cf67bb6d3f5e9368a8296e570092
Subhendu Sekhar Sahu <116280639+subhendu1239@users.noreply.github.com>
2026-01-27 13:20:35 +0530

    new changes

--- src/core/StringDupCount.java
+++ src/core/StringDupCount.java
@@ -2,6 +2,7 @@ package core;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -14,5 +15,14 @@ public class StringDupCount {
 		Map<String, Long> namesCount = names.stream().filter(x -> Collections.frequency(names, x) >= 1)
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 		System.out.println(namesCount);
+		
+		//or
+		LinkedHashMap<String, Long> collect = names.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(e->e.getValue()>=1l)
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2)->e1, LinkedHashMap::new));
+		System.out.println(collect);
 	}
 }
